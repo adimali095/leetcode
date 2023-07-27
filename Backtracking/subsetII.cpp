2cd88020029acfadf63fb437b3f06630abc32f72
adimali095 <72328713+adimali095@users.noreply.github.com>
2023-07-27 09:30:07 +0530

    Updated subsetII problem
    Delete unecessay functions

--- Backtracking/subsetII.cpp
+++ Backtracking/subsetII.cpp
@@ -12,38 +12,6 @@ public:
         temp.pop_back();
         solve(nums,res,temp,i+1);
     }
-    void removeElement(vector<vector<int>> &num,vector<int> eleRemove)
-    {
-        num.erase( find(num.begin(),num.end(),eleRemove) );
-    }
-    vector<vector<int>> delDuplicates(vector<vector<int>> vec)
-    {
-        vector<vector<int>> temp;
-        for(int i =0;i<vec.size()-1;i++)
-        {
-            bool isSame = true;
-            if(vec[i].size() == vec[i+1].size())
-            {
-                for(int j =0;j<vec[i].size();j++)
-                {
-                    if(vec[i][j] != vec[i+1][j])
-                    {
-                        isSame = false;
-                        break;
-                    }
-                }
-                if(isSame)
-                {
-                    temp.push_back(vec[i]);
-                }
-            }
-        }
-        for(auto i : temp)
-        {
-            removeElement(vec,i);
-        }
-        return vec;
-    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums){
         if(nums.empty())
         {
